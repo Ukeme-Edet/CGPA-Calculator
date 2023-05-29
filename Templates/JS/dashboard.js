2088b078256125271c50970b16691cf7d0226799
Ukeme Edet <ukemeedet2207@gmail.com>
2023-05-29 23:58:49 +0100

    FIxed a broken link in the dashbord page and a selector in the js

--- Templates/JS/dashboard.js
+++ Templates/JS/dashboard.js
@@ -1,9 +1,9 @@
 // to make the add courses form show
 const addCourseContainer = document.querySelector("#addCourseContainer");
 const addCourseForm = document.querySelector("#addCourseForm");
-const addCourseBackground = document.querySelector("#container")
-const addEditBtn = document.querySelector("#addEditCourseBtn")
-const courseTable = document.querySelector("#courseTable")
+const addCourseBackground = document.querySelector("#container");
+const addEditBtn = document.querySelector("#addEditCourseBtn");
+const courseTable = document.querySelector("#courseTable");
 
 document.querySelector("#addCourseBtn").addEventListener("click", () => {
     addCourseContainer.classList.add("form--hidden");
@@ -12,7 +12,7 @@ document.querySelector("#addCourseBtn").addEventListener("click", () => {
     addEditBtn.classList.remove("form--hidden");
 });
 
-document.querySelector("#addCourseFormBtn").addEventListener("click", e => {
+document.querySelector(".addCourseFormBtn").addEventListener("click", (e) => {
     e.preventDefault();
     courseTable.classList.remove("form--hidden");
     addCourseForm.classList.add("form--hidden");
@@ -21,25 +21,23 @@ document.querySelector("#addCourseFormBtn").addEventListener("click", e => {
 });
 
 // To append data to table
-function addCourse()
-{
+function addCourse() {
     var courseTitle = document.sample.courseTitle.value;
     var courseCode = document.sample.courseCode.value;
     var courseUnit = document.sample.courseUnit.value;
     var courseGrade = document.sample.courseGrade.value;
 
-    var tr = document.createElement('tr');
-    var td1 = tr.appendChild(document.createElement('td'));
-    var td2 = tr.appendChild(document.createElement('td'));
-    var td3 = tr.appendChild(document.createElement('td'));
-    var td4 = tr.appendChild(document.createElement('td'));
-    var td5 = tr.appendChild(document.createElement('td'));
+    var tr = document.createElement("tr");
+    var td1 = tr.appendChild(document.createElement("td"));
+    var td2 = tr.appendChild(document.createElement("td"));
+    var td3 = tr.appendChild(document.createElement("td"));
+    var td4 = tr.appendChild(document.createElement("td"));
+    var td5 = tr.appendChild(document.createElement("td"));
 
-    td2.innerHTML=courseTitle;
-    td3.innerHTML=courseCode;
-    td4.innerHTML=courseUnit;
-    td5.innerHTML=courseGrade;
+    td2.innerHTML = courseTitle;
+    td3.innerHTML = courseCode;
+    td4.innerHTML = courseUnit;
+    td5.innerHTML = courseGrade;
 
     document.getElementById("tb1").appendChild(tr);
-    
 }
